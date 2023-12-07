7d663cfc98125993a4751d74218ff987def5c027
DevopsLoverArchana <151740379+DevopsLoverArchana@users.noreply.github.com>
2023-12-07 15:42:34 +0530

    Add files via upload

---
+++ src/main/java/com/example/mavenspringbootproject1/Mavenspringbootproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.mavenspringbootproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mavenspringbootproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mavenspringbootproject1Application.class, args);
+	}
+
+}
